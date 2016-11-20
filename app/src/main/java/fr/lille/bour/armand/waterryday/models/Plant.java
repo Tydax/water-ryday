fce003f71184467319aa87839652a21d1b9df060
Armand (Tydax) Bour <tydaxx@gmail.com>
2016-11-20 18:28:28 +0100

    Initialise workflow with Android project

---
+++ app/src/main/java/fr/lille/bour/armand/waterryday/models/Plant.java
@@ -0,0 +1,14 @@
+package fr.lille.bour.armand.waterryday.models;
+
+/**
+ * The {@link Plant} class represents a plant saved by the user in the application.
+ *
+ * @author Armand (Tydax) BOUR
+ */
+
+public class Plant {
+
+    /** The plant's name. */
+    protected String name;
+
+}
